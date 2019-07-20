844803a052653d68d1baacc11d441ba3f7be1487
irconde <ivanrodriguezconde@gmail.com>
2019-07-20 11:03:23 +0200

    Menu replaced by bottom button

--- app/src/main/java/com/acme/tictactoe/view/TicTacToeActivity.java
+++ app/src/main/java/com/acme/tictactoe/view/TicTacToeActivity.java
@@ -1,10 +1,9 @@
 package com.acme.tictactoe.view;
 
 import androidx.databinding.DataBindingUtil;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -40,23 +39,6 @@ public class TicTacToeActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         viewModel.onDestroy();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_tictactoe, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_reset:
-                viewModel.onResetSelected();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
 }
